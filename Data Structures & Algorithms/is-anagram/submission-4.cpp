9d0a240259debc46b0a59161f1f982a4af952a63
vishnusrinivas00 <143373649+vishnusrinivas00@users.noreply.github.com>
2026-06-05 16:57:30 +0530

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size()){
+            return false;
+        }
+        vector<int>freq(26,0);
+        for(char c:s){freq[c-'a']++;}
+        for(char c:t){freq[c-'a']--;}
+        
+        for(int i:freq){
+            if(i!=0){return false;}
+        }
+        return true;
+    }
+};
